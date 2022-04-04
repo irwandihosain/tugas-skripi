47c274b7768841d4fc0533a13ae083a4fba2c068
unknown <dnseamangasa@gmail.com>
2022-04-04 17:52:41 +0800

    aaa

--- mysql/tugas-skripsi-1.sql
+++ mysql/tugas-skripsi-1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Apr 2022 pada 11.51
+-- Waktu pembuatan: 04 Apr 2022 pada 11.44
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 8.1.2
 
